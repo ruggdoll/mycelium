e774932211a353692339379be7fea79103bba5b0
cpt_quintal <ruggdoll@mailo.com>
2026-04-27 01:55:44 +0200

    Update URL format in fetch_sub function

--- lib/rhizomorphe/lib_urlscan.py
+++ lib/rhizomorphe/lib_urlscan.py
@@ -4,7 +4,7 @@ import re
 def fetch_sub(domain):
     session = requests.session()
     session.headers = {"User-Agent": "Mozilla/5.0"}
-    url = "https://urlscan.io/api/v1/search/?q={}".format(domain)
+    url = "https://urlscan.io/api/v1/search/?q=domain:{}&size=100".format(domain)
     req = session.get(url, timeout=20)
     req.raise_for_status()
     pattern = r'url\": \"https://(.*?)\.' + re.escape(domain)
